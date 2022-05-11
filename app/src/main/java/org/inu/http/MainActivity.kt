00467e2f9d2b7bf2edaa1e35fa52812196d756ce
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-12 01:09:32 +0900

    #1 - Thread + HttpUrlConnection

--- app/src/main/java/org/inu/http/MainActivity.kt
+++ app/src/main/java/org/inu/http/MainActivity.kt
@@ -1,11 +1,59 @@
 package org.inu.http
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    private val buttonRequest: AppCompatButton by lazy {
+        findViewById(R.id.buttonRequest)
+    }
+    private val editUrl: EditText by lazy {
+        findViewById(R.id.editUrl)
+    }
+    private val textContent: TextView by lazy {
+        findViewById(R.id.textContent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonRequest.setOnClickListener {
+            thread(start = true) {
+                try {
+                    var urlText = editUrl.text.toString()
+                    if (!urlText.startsWith("https")) {
+                        urlText = "https://${urlText}"
+                    }
+                    val url = URL(urlText)
+                    val urlConnection = url.openConnection() as HttpURLConnection
+                    urlConnection.requestMethod = "GET"
+                    if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
+                        val streamReader = InputStreamReader(urlConnection.inputStream)
+                        val buffered = BufferedReader(streamReader)
+                        val content = StringBuilder()
+                        while (true) {
+                            val line = buffered.readLine() ?: break
+                            content.append(line)
+                        }
+                        buffered.close()
+                        urlConnection.disconnect()
+                        runOnUiThread {
+                            textContent.text = content.toString()
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
