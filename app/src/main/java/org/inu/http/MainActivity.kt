fc0fb297271cd56b2a8ac2d4a3f9db6c23ba0745
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-12 01:25:34 +0900

    #1 - Coroutine + HttpUrlConnection

--- app/src/main/java/org/inu/http/MainActivity.kt
+++ app/src/main/java/org/inu/http/MainActivity.kt
@@ -5,11 +5,11 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import kotlinx.coroutines.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private val buttonRequest: AppCompatButton by lazy {
@@ -27,32 +27,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         buttonRequest.setOnClickListener {
-            thread(start = true) {
-                try {
-                    var urlText = editUrl.text.toString()
-                    if (!urlText.startsWith("https")) {
-                        urlText = "https://${urlText}"
-                    }
-                    val url = URL(urlText)
-                    val urlConnection = url.openConnection() as HttpURLConnection
-                    urlConnection.requestMethod = "GET"
-                    if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
-                        val streamReader = InputStreamReader(urlConnection.inputStream)
-                        val buffered = BufferedReader(streamReader)
-                        val content = StringBuilder()
-                        while (true) {
-                            val line = buffered.readLine() ?: break
-                            content.append(line)
-                        }
-                        buffered.close()
-                        urlConnection.disconnect()
-                        runOnUiThread {
-                            textContent.text = content.toString()
-                        }
-                    }
-                } catch (e: Exception) {
-                    e.printStackTrace()
+
+            val exceptionHandler = CoroutineExceptionHandler { context, throwable ->
+                println("CoroutineExceptionHandler : $throwable")
+                textContent.text = "Exception 발생!(올바른 URL을 적어주세요)"
+            }
+            val content = CoroutineScope(Dispatchers.IO).async {
+                var urlText = editUrl.text.toString()
+                if (!urlText.startsWith("https")) {
+                    urlText = "https://${urlText}"
                 }
+                val url = URL(urlText)
+                val urlConnection = url.openConnection() as HttpURLConnection
+                urlConnection.requestMethod = "GET"
+                val content = StringBuilder()
+                if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
+                    val streamReader = InputStreamReader(urlConnection.inputStream)
+                    val buffered = BufferedReader(streamReader)
+                    while (true) {
+                        val line = buffered.readLine() ?: break
+                        content.append(line)
+                    }
+                    buffered.close()
+                    urlConnection.disconnect()
+                }
+                content
+            }
+            CoroutineScope(Dispatchers.Main+exceptionHandler).launch{
+                textContent.text = content.await()
+                println(34535435)
+            }
+            CoroutineScope(Dispatchers.Main).launch{
+                println(12312321)
             }
         }
     }
