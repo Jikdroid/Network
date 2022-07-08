53ea86d4aac7e1e9e4ccd7bb2dfd39b76142e59b
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-07-08 19:33:23 +0900

    #6 - Add error handling in RecyclerView

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -28,17 +28,21 @@ import retrofit2.Call
 import retrofit2.Response
 import javax.security.auth.callback.Callback
 
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
+
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the most recent response
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the response String
-    val response: LiveData<String>
-        get() = _response
+    val status: LiveData<MarsApiStatus>
+        get() = _status
 
     private val _properties = MutableLiveData<List<MarsProperty>>()
     val properties : LiveData<List<MarsProperty>>
@@ -54,13 +58,15 @@ class OverviewViewModel : ViewModel() {
     /**
      * Sets the value of the status LiveData to the Mars API status.
      */
-    private fun getMarsRealEstateProperties() {
+    private fun getMarsRealEstateProperties() {     // 시작할 때만 호출
        viewModelScope.launch {
+           _status.value = MarsApiStatus.LOADING
            try {
+               _status.value = MarsApiStatus.DONE
                _properties.value = MarsApi.retrofitService.getProperties()
-               _response.value = "Success: Mars properties retrieved"
            } catch (e: Exception){
-               _response.value = "Failure: ${e.message}"
+               _status.value = MarsApiStatus.ERROR
+               _properties.value = ArrayList()
            }
        }
     }
