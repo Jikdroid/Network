96c6c76988e6bc221fc1bc205374d7dcc8b94f6a
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-07-08 17:35:54 +0900

    #6 - Display an internet image

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -23,5 +23,5 @@ data class MarsProperty(
     val id: String,
     val type: String,
     val price: Double,
-    @Json(name = "img_src") val img_src: String
+    @Json(name = "img_src") val imgSrcUrl: String
 )
