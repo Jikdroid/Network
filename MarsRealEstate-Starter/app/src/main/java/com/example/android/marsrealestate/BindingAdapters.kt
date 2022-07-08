0e5f7b30f161c5efff0f738b892a04885ec5df6b
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-07-08 18:58:23 +0900

    #6 - Display a grid of images with a RecyclerView

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -20,8 +20,11 @@ package com.example.android.marsrealestate
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl:String?){
@@ -34,4 +37,10 @@ fun bindImage(imgView: ImageView, imgUrl:String?){
                 .error(R.drawable.ic_broken_image))     // 에러가 떴을 떄 표시할 이미지
             .into(imgView)
     }
+}
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsProperty>?){
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
 }
